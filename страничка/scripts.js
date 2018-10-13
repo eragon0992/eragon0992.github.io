4e31104933b11df4feab8c5fcbb185d1ad27b3b0
eragon0992 <piligrim.ya@gmail.com>
2018-10-13 14:22:36 +0300

    update

--- страничка/scripts.js
+++ страничка/scripts.js
@@ -1,4 +1,45 @@
-$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-    e.target // newly activated tab
-    e.relatedTarget // previous active tab
-})
+var list = [
+	'Яблоко',
+	'Груша',
+	'Апельсин'
+];
+
+
+document.getElementById('Subscribe').onclick=function(){
+	var elementList = document.getElementById('list');
+	var fname = document.getElementById('inputFirstname');
+	var lname = document.getElementById('inputLastname');
+	var email = document.getElementById('SubscribeEmail');
+	
+	lname.classList.remove('is-invalid');
+	fname.classList.remove('is-invalid');
+	email.classList.remove('is-invalid');
+				
+	var string ='';
+	
+	if((fname.value!='')&&lname.value!=''){
+		if(email.value.indexOf('@')>=0 && email.value.indexOf('.')>=0){
+				elementList.innerHTML = 
+				'<li>'+ fname.value + '</li>' + 
+				'<li>'+ lname.value + '</li>' +
+				'<li>'+ email.value + '</li>';
+				fname.value='';
+				lname.value='';
+				email.value='';
+				
+				
+		}
+				else{
+					email.classList.add('is-invalid');
+				}
+	}
+	else{
+		fname.classList.add('is-invalid');
+		lname.classList.add('is-invalid');
+	}
+};
+
+
+
+var elementList = document.getElementById('list');
+	elementList.innerHTML ='<li>' + list.join('</li><li>')+ '</li>';
