c5e6c1555522525de9690615d637019f37463cfe
eragon0992 <piligrim.ya@gmail.com>
2018-10-24 20:38:54 +0300

    Add files via upload

--- MyProject/scripts.js
+++ MyProject/scripts.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            $('#row1').addClass('animated fadeInUp');
-            $('#row1').css("visibility", "visible");
-        }
-    });
+            $('#HeaderLogo').addClass('animated fadeInLeft');
+            $('#HeaderTitle').addClass('animated fadeInRight');
+            $('#HeaderLogo').css("visibility", "visible");
+            $('#HeaderTitle').css("visibility", "visible");
+});
+
+$("#Title").hover(function(){
+	$("#Title").addClass('animated pulse');
+	$('#HeaderTitle').css("visibility", "visible");
 })
+
+$('.carousel').carousel({
+  interval: 3500
+})
+
+$('.carousel-control-prev-icon').click(function() {
+  $('#carouselExampleFade').carousel('prev');
+});
+
+$('.carousel-control-next-icon').click(function() {
+  $('#carouselExampleFade').carousel('next');
+});
