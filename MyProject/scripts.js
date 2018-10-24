dfae3e2676b5d7b9ee509048237a202c9193e28e
eragon0992 <piligrim.ya@gmail.com>
2018-10-24 23:54:38 +0300

    animated

--- MyProject/scripts.js
+++ MyProject/scripts.js
@@ -1,23 +1,26 @@
+AOS.init();
 $(document).ready(function() {
-            $('#HeaderLogo').addClass('animated fadeInLeft');
-            $('#HeaderTitle').addClass('animated fadeInRight');
-            $('#HeaderLogo').css("visibility", "visible");
-            $('#HeaderTitle').css("visibility", "visible");
+    $('#HeaderLogo').addClass('animated fadeInLeft');
+    $('#HeaderTitle').addClass('animated fadeInRight');
+    $('#HeaderLogo').css("visibility", "visible");
+    $('#HeaderTitle').css("visibility", "visible");
 });
 
-$("#Title").hover(function(){
-	$("#Title").addClass('animated pulse');
-	$('#HeaderTitle').css("visibility", "visible");
+$("#Title").hover(function() {
+    $("#Title").addClass('animated pulse');
+    $('#HeaderTitle').css("visibility", "visible");
 })
 
 $('.carousel').carousel({
-  interval: 3500
+    interval: 3500
 })
 
 $('.carousel-control-prev-icon').click(function() {
-  $('#carouselExampleFade').carousel('prev');
+    $('#carouselExampleFade').carousel('prev');
 });
 
 $('.carousel-control-next-icon').click(function() {
-  $('#carouselExampleFade').carousel('next');
+    $('#carouselExampleFade').carousel('next');
 });
+
+$('.dropdown-toggle').dropdown();
