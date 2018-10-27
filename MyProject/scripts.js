b6e281df13ad0fec30c35b5a74ddbec775fe412b
eragon0992 <piligrim.ya@gmail.com>
2018-10-27 21:14:38 +0300

    Update

--- MyProject/scripts.js
+++ MyProject/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $('#HeaderTitle').addClass('animated fadeInRight');
     $('#HeaderLogo').css("visibility", "visible");
     $('#HeaderTitle').css("visibility", "visible");
+
 });
 
 $("#Title").hover(function() {
@@ -24,3 +25,31 @@ $('.carousel-control-next-icon').click(function() {
 });
 
 $('.dropdown-toggle').dropdown();
+
+document.getElementById('btnSubmit').onclick = function() {
+    var name = document.getElementById('inputName');
+    var email = document.getElementById('exampleInputEmail1');
+    var message = document.getElementById('exampleFormControlTextarea1');
+    var text = '';
+    name.classList.remove('is-invalid');
+    email.classList.remove('is-invalid');
+    message.classList.remove('is-invalid');
+
+    if (name.value.length < 2 || email.value.length < 2 || message.value.length < 2 || email.value.indexOf('@') < 0) {
+        if (name.value.length < 2) {
+            name.classList.add('is-invalid');
+        }
+        if (message.value.length < 2) {
+            message.classList.add('is-invalid');
+        }
+        if (email.value.length < 2 || email.value.indexOf('@') < 0) {
+            email.classList.add('is-invalid');
+        }
+    } else {
+        text += 'Доброго дня, ' + name.value + '! \n Ми зв\'яжемося з вами найближчим часом. Відповідь надішлемо на Вашу поштову скриньку: ' + email.value + '.';
+        name.value = '';
+        email.value = '';
+        message.value = '';
+        alert(text);
+    }
+};
